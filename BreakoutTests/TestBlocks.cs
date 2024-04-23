68d23443d3dce181de4ff76761d5fc7bd7ae38c1
Elias Nielsen <eli2003@outlook.dk>
2024-04-23 22:17:15 +0200

    Initial commit, setup done

---
+++ BreakoutTests/TestBlocks.cs
@@ -0,0 +1,15 @@
+namespace BreakoutTests;
+
+public class TestBlocks
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
