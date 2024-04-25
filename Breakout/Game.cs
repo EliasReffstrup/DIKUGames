20a817d9bba7ccb108e0ba2714b053574be716d0
Elias Nielsen <eli2003@outlook.dk>
2024-04-25 17:48:42 +0200

    added the level loader class, along with LevelData datatype. Loader returns this LevelData ADT

--- Breakout/Game.cs
+++ Breakout/Game.cs
@@ -4,11 +4,12 @@ using System;
 using DIKUArcade;
 using DIKUArcade.GUI;
 using DIKUArcade.Input;
+using LevelLoading;
 
 public class Game : DIKUGame
 {
     private BlockContainer container = new BlockContainer();
-
+    private LoadLevel loader = new LoadLevel();
 
 
     public Game(WindowArgs windowArgs) : base(windowArgs)
@@ -27,6 +28,10 @@ public class Game : DIKUGame
         {
             case KeyboardKey.Escape:
                 window.CloseWindow();
+                break;
+            
+            case KeyboardKey.Space:
+                loader.printLevelDataToConsole(loader.ReadLevelFile("level3.txt"));
                 break;
         }
     }
---
+++ Breakout/LevelLoading/LevelData.cs
@@ -0,0 +1,7 @@
+namespace Breakout.LevelLoading;
+
+public class LevelData {
+    public string MapSection { get; set; }
+    public Dictionary<string, string> MetaDictionary { get; set; }
+    public Dictionary<string, string> LegendDictionary { get; set; }
+}
